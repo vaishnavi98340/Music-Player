41b14516029623970c4a6057eba4d0d5a0b22d74
vaishnavi98340 <161127691+vaishnavi98340@users.noreply.github.com>
2024-03-21 17:59:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,350 @@
+document.querySelector(".btn1").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn1=new Audio("deva deva.mp3");
+    btn1.play();
+})
+document.querySelector(".btn2").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn1=new Audio("deva deva.mp3");
+    btn1.pause();
+})
+document.querySelector(".btn3").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn3=new Audio("Srivalli - Pushpa - The Rise 320 Kbps.mp3");
+    btn1.play();
+})
+document.querySelector(".btn4").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn4=new Audio("Srivalli - Pushpa - The Rise 320 Kbps.mp3");
+    btn1.pause();
+})
+document.querySelector(".btn5").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn5=new Audio("What Jhumka_192(PagalWorld.com.cm).mp3");
+    btn1.play();
+})
+document.querySelector(".btn6").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn6=new Audio("What Jhumka_192(PagalWorld.com.cm).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn7").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn7=new Audio("Lutt Putt Gaya Dunki 320 Kbps.mp3");
+    btn1.play();
+})
+document.querySelector(".btn8").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn8=new Audio("Lutt Putt Gaya Dunki 320 Kbps.mp3");
+    btn1.pause();
+})
+document.querySelector(".btn9").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn9=new Audio("Naiyo Lagda Dil Mera.mp3");
+    btn1.play();
+})
+document.querySelector(".btn10").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn10=new Audio("Naiyo Lagda Dil Mera.mp3");
+    btn1.pause();
+})
+document.querySelector(".btn11").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn11=new Audio("");
+    btn1.play();
+})
+document.querySelector(".btn12").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn12=new Audio("");
+    btn1.pause();
+})
+document.querySelector(".btn13").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn13=new Audio("Mann Bharryaa 2.0 - Shershaah 128 Kbps.mp3");
+    btn1.play();
+})
+document.querySelector(".btn14").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn14=new Audio("Mann Bharryaa 2.0 - Shershaah 128 Kbps.mp3");
+    btn1.pause();
+})
+document.querySelector(".btn15").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn15=new Audio("c:\Users\icon\Downloads\Current Laga Re_192(PagalWorld.com.cm).mp3");
+    btn1.play();
+})
+document.querySelector(".btn16").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn16=new Audio("c:\Users\icon\Downloads\Current Laga Re_192(PagalWorld.com.cm).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn17").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn17=new Audio("c:\Users\icon\Downloads\Chaleya(PagalWorld.com.cm).mp3");
+    btn1.play();
+})
+document.querySelector(".btn18").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn18=new Audio("c:\Users\icon\Downloads\Chaleya(PagalWorld.com.cm).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn19").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn19=new Audio("Dance ka Bhoot_64(PagalWorld.com.cm).mp3");
+    btn1.play();
+})
+document.querySelector(".btn20").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn20=new Audio("Dance ka Bhoot_64(PagalWorld.com.cm).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn21").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn21=new Audio("Kesariya_64(PagalWorld.com.cm) (1).mp3");
+    btn1.play();
+})
+document.querySelector(".btn22").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn22=new Audio("Kesariya_64(PagalWorld.com.cm) (1).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn23").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn23=new Audio("Filhall B Praak 320 Kbps.mp3");
+    btn1.play();
+})
+document.querySelector(".btn24").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn24=new Audio("Filhall B Praak 320 Kbps.mp3");
+    btn1.pause();
+})
+document.querySelector(".btn25").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn25=new Audio("_Heeriye(PagalWorld.com.cm).mp3");
+    btn1.play();
+})
+document.querySelector(".btn26").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn26=new Audio("_Heeriye(PagalWorld.com.cm).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn27").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn27=new Audio("c:\Users\icon\Downloads\Koi Si(PagalWorld.com.cm).mp3");
+    btn1.play();
+})
+document.querySelector(".btn28").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn28=new Audio("c:\Users\icon\Downloads\Koi Si(PagalWorld.com.cm).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn29").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn29=new Audio("c:\Users\icon\Downloads\Ve Kamleya Female Version_128-(PagalWorld).mp3");
+    btn1.play();
+})
+document.querySelector(".btn30").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn30=new Audio("c:\Users\icon\Downloads\Ve Kamleya Female Version_128-(PagalWorld).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn31").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn31=new Audio("O Mahi O Mahi_64(PagalWorld.com.cm).mp3");
+    btn1.play();
+})
+document.querySelector(".btn32").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn32=new Audio("O Mahi O Mahi_64(PagalWorld.com.cm).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn33").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn33=new Audio("c:\Users\icon\Downloads\Tum Kya Mile_192(PagalWorld.com.cm).mp3");
+    btn1.play();
+})
+document.querySelector(".btn34").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn34=new Audio("c:\Users\icon\Downloads\Tum Kya Mile_192(PagalWorld.com.cm).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn35").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn35=new Audio("c:\Users\icon\Downloads\Raabta Agent Vinod 320 Kbps.mp3");
+    btn1.play();
+})
+document.querySelector(".btn36").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn36=new Audio("c:\Users\icon\Downloads\Raabta Agent Vinod 320 Kbps.mp3");
+    btn1.pause();
+})
+document.querySelector(".btn37").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn37=new Audio("c:\Users\icon\Downloads\Hua Main - Animal 128 Kbps.mp3");
+    btn1.play();
+})
+document.querySelector(".btn38").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn38=new Audio("c:\Users\icon\Downloads\Hua Main - Animal 128 Kbps.mp3");
+    btn1.pause();
+})
+document.querySelector(".btn39").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn39=new Audio("c:\Users\icon\Downloads\Raanjhanaa - Raanjhanaa 128 Kbps.mp3");
+    btn1.play();
+})
+document.querySelector(".btn40").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn40=new Audio("c:\Users\icon\Downloads\Raanjhanaa - Raanjhanaa 128 Kbps.mp3");
+    btn1.pause();
+})
+document.querySelector(".btn41").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn41=new Audio("c:\Users\icon\Downloads\Adhir-Man-Jhale(PagalWorld).mp3");
+    btn1.play();
+})
+document.querySelector(".btn42").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn42=new Audio("c:\Users\icon\Downloads\Adhir-Man-Jhale(PagalWorld).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn43").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn43=new Audio("Butterfly-(320Kbps.Com.In).mp3");
+    btn1.play();
+})
+document.querySelector(".btn44").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn44=new Audio("Butterfly-(320Kbps.Com.In).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn45").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn45=new Audio("c:\Users\icon\Downloads\Mitwaa(PagalWorld).mp3");
+    btn1.play();
+})
+document.querySelector(".btn46").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn46=new Audio("c:\Users\icon\Downloads\Mitwaa(PagalWorld).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn47").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn47=new Audio("Gaav Sutana.mp3");
+    btn1.play();
+})
+document.querySelector(".btn48").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn48=new Audio("Gaav Sutana.mp3");
+    btn1.pause();
+})
+document.querySelector(".btn49").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn49=new Audio("c:\Users\icon\Downloads\Jhumka(PagalWorlld.Com).mp3");
+    btn1.play();
+})
+document.querySelector(".btn50").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn50=new Audio("c:\Users\icon\Downloads\Jhumka(PagalWorlld.Com).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn51").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn51=new Audio("Kajwa(PagalWorldl).mp3");
+    btn1.play();
+})
+document.querySelector(".btn52").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn52=new Audio("Kajwa(PagalWorldl).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn53").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn53=new Audio("c:\Users\icon\Downloads\Kevadyacha-Paan-Tu(PagalWorldl).mp3");
+    btn1.play();
+})
+document.querySelector(".btn54").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn54=new Audio("c:\Users\icon\Downloads\Kevadyacha-Paan-Tu(PagalWorldl).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn55").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn55=new Audio("c:\Users\icon\Downloads\Ved Lavlay_320(PagalWorld.com.cm).mp3");
+    btn1.play();
+})
+document.querySelector(".btn56").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn56=new Audio("c:\Users\icon\Downloads\Ved Lavlay_320(PagalWorld.com.cm).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn57").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn57=new Audio("c:\Users\icon\Downloads\Painjan(PagalWorld).mp3");
+    btn1.play();
+})
+document.querySelector(".btn58").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn58=new Audio("c:\Users\icon\Downloads\Painjan(PagalWorld).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn59").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn59=new Audio("c:\Users\icon\Downloads\Sukh Kalale_320(PagalWorld.com.cm).mp3");
+    btn1.play();
+})
+document.querySelector(".btn60").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn60=new Audio("c:\Users\icon\Downloads\Sukh Kalale_320(PagalWorld.com.cm).mp3");
+    btn1.pause();
+})
+document.querySelector(".btn61").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn61=new Audio("new_128_01 - Dil To Pagal Hai - DTPH 1997.mp3");
+    btn1.play();
+})
+document.querySelector(".btn62").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn62=new Audio("new_128_01 - Dil To Pagal Hai - DTPH 1997.mp3");
+    btn1.pause();
+})
+document.querySelector(".btn63").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn63=new Audio("c:\Users\icon\Downloads\Hawa Hawai Mr India 320 Kbps.mp3");
+    btn1.play();
+})
+document.querySelector(".btn64").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn64=new Audio("c:\Users\icon\Downloads\Hawa Hawai Mr India 320 Kbps.mp3");
+    btn1.pause();
+})
+document.querySelector(".btn65").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn65=new Audio("c:\Users\icon\Downloads\bollywood_Beta 1992 - Koyal Si Teri Boli.mp3");
+    btn1.play();
+})
+document.querySelector(".btn66").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn66=new Audio("c:\Users\icon\Downloads\bollywood_Beta 1992 - Koyal Si Teri Boli.mp3");
+    btn1.pause();
+})
+document.querySelector(".btn67").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn67=new Audio("c:\Users\icon\Downloads\bollywood_MI 1987 - Mr. India Parody.mp3");
+    btn1.play();
+})
+document.querySelector(".btn68").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn68=new Audio("c:\Users\icon\Downloads\bollywood_MI 1987 - Mr. India Parody.mp3");
+    btn1.pause();
+})
+document.querySelector(".btn69").addEventListener("click",function(){
+    alert("I got a click play");
+    var btn69=new Audio("Neend Churayee Meri - Ishq 128 Kbps.mp3");
+    btn1.play();
+})
+document.querySelector(".btn70").addEventListener("click",function(){
+    alert("I got a click pause");
+    var btn70=new Audio("Neend Churayee Meri - Ishq 128 Kbps.mp3");
+    btn1.pause();
+})
